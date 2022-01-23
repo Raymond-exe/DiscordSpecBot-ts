2a4d3512f92456f88c958eeaf6b18b109a2dfa35
Raymond Wong <raymondwong1701@gmail.com>
2022-01-23 15:48:40 -0800

    .gitignore & foundations

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { Client, Intents } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+    intents: [ 
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+client.on('ready', () => {
+    console.log('Bot is ready.');
+});
+
+client.on('messageCreate', (message) => {
+    console.log(`Message received! Type: ${typeof message}\nContents: ${message.content}`);
+})
+
+client.login(process.env.TOKEN);
---
+++ src/commands/command.ts
@@ -0,0 +1,46 @@
+export class Command {
+    public name: string;
+    public description: string;
+    public caseSensitive: boolean;
+    public aliases: string[];
+    public action: Function;
+    
+    constructor(name: string, aliases: string[] = [], caseSensitive: boolean = false) {
+        this.name = caseSensitive ? name : name.toLowerCase();
+        this.aliases = caseSensitive ? aliases : toLowerCase(aliases);
+        this.caseSensitive = caseSensitive;
+
+        function toLowerCase(strs: string[]): string[] {
+            let out = [];
+            strs.forEach(str => {
+                out.push(str.toLowerCase());
+            })
+
+            return out;
+        }
+    }
+
+    public setAction(action: Function): Command {
+        this.action = action;
+        return this;
+    }
+
+    public setDescription(desc: string): Command {
+        this.description = desc;
+        return this;
+    }
+
+    public match(phrase: string): boolean {
+        const caps = this.caseSensitive;
+
+        phrase = caps ? phrase : phrase.toLowerCase();
+
+        if(phrase === this.name) {
+            return true;
+        } else if (this.aliases.includes(phrase)) {
+            return true;
+        }
+
+        return false;
+    }
+}
