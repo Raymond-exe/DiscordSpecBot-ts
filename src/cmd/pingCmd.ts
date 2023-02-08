26d601381731717fa09efcc435f1cbb81223b3d3
Raymond Wong <raymondwong1701@gmail.com>
2023-02-08 08:50:32 -0800

    Debug logic for ping command

--- src/cmd/pingCmd.ts
+++ src/cmd/pingCmd.ts
@@ -4,7 +4,9 @@ module.exports = {
     data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription(':ping_pong: Ping-pong!'),
-    execute: async (interaction) => {
+    execute: async (interaction) => { // TODO find out what class interaction is
         await interaction.reply(':ping_pong: Pong!')
+        console.log(`Interaction class: ${typeof interaction} | ${interaction.class}`);
+        console.log(`Interaction keys: ${Object.keys(interaction)}`);
     }
 }
