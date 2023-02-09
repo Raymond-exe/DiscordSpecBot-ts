0aea4473fab0ce3fb1aa7246d7da1aee2f4387e3
Raymond Wong <raymondwong1701@gmail.com>
2023-02-09 13:19:52 -0800

    added caching to search command

--- src/cmd/searchCmd.ts
+++ src/cmd/searchCmd.ts
@@ -1,7 +1,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { searchSteamApps } from '../games/steam';
+import { Message } from 'discord.js';
+import { searchSteamApps, SteamGame } from '../games/steam';
 
-const QUERY_COUNT = 10;
+const QUERY_COUNT = 25;
+const RESULTS_PER_PAGE = 5;
+
+const SEARCH_CACHE: Map<String, SteamGame[]> = new Map();
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -18,8 +22,14 @@ module.exports = {
         .setDescription('The name of hardware or game to search for.')
         .setRequired(true)
     ),
-    execute: async (interaction) => {
-        const results = searchSteamApps(interaction.content, QUERY_COUNT);
+    execute: async (interaction: Message) => {
+        const query: string = interaction.content;
+
+        if (!SEARCH_CACHE.has(query.toLowerCase())) {
+            SEARCH_CACHE.set(query.toLowerCase(), searchSteamApps(query, QUERY_COUNT));
+        }
+        const results = SEARCH_CACHE.get(query);
+
         await interaction.reply(JSON.stringify(results));
     }
 }
