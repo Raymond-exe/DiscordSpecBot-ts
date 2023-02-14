543da307469490982cf78f02a9cf6749a2c32f98
Raymond Wong <raymondwong1701@gmail.com>
2023-02-14 13:37:36 -0800

    Specified type for interaction in help cmd

--- src/cmd/helpCmd.ts
+++ src/cmd/helpCmd.ts
@@ -1,10 +1,11 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { Message } from 'discord.js';
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('Default help command for SpecBot'), // TODO write description
-    execute: async (interaction) => {
+    execute: async (interaction: Message) => {
         await interaction.reply('kek');
         // TODO make a help thingy
     }
