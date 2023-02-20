c187079f8cfbeb1440a4ee667e664dc3541a45e1
Raymond Wong <raymondwong1701@gmail.com>
2023-02-20 00:34:11 -0800

    getting message params in setRamCmd

--- src/cmd/setRamCmd.ts
+++ src/cmd/setRamCmd.ts
@@ -1,14 +1,18 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Message } from 'discord.js';
+import { getMessageParameters } from './command';
 
 module.exports = {
     data: new SlashCommandBuilder().setName('setram')
     .setDescription('Assigns the RAM in gb for your PC build.')
-    .addStringOption(option =>
-        option.setName('gb')
-        .setDescription('Amount of RAM in GB')
+    .addIntegerOption(option =>
+        option.setName('amount')
+        .setDescription('Your amount of RAM in GB')
         .setRequired(true)),
     execute: async (interaction: Message) => {
-        await interaction.reply('ye ram');
+        const parameters = getMessageParameters(interaction);
+        const ram = parameters['amount']; // int
+
+        await interaction.reply(`You entered ${ram} ram`);
     }
 }
