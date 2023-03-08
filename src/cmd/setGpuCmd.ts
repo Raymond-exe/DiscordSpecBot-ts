ab1af6be9189f73cef202e678c6258f8e69a2b78
Raymond Wong <raymondwong1701@gmail.com>
2023-03-08 14:51:56 -0800

    Progress on setgpu command

--- src/cmd/setGpuCmd.ts
+++ src/cmd/setGpuCmd.ts
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Message } from 'discord.js';
 import { Parse } from '../hardware/utils';
+import { getMessageParameters } from './command';
 
 module.exports = {
     data: new SlashCommandBuilder().setName('setgpu')
@@ -10,11 +11,17 @@ module.exports = {
         .setDescription('Name of your GPU')
         .setRequired(true)),
     execute: async (interaction: Message) => {
-        const name = ""; // TODO get GPU Name option
+        const parameters = getMessageParameters(interaction);
+        const name = parameters['gpu'];
         const GPU = await Parse.cpu(name);
 
         // TODO assign GPU to user's build
+        const success = false;
 
-        await interaction.reply('GPU kek');
+        await interaction.reply(success ? `Set your GPU to ${GPU.name}` : `Failed to set your GPU as ${GPU.name}, please try again shortly!`);
+
+        if (!success) {
+            // TODO error logging
+        }
     }
 }
